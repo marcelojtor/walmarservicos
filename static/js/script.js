6060becbd27c52aa663b58b821eec0534e7adcb5
marcelojtor <marcelo.jtorres1975@gmail.com>
2026-03-18 08:47:11 -0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,11 @@ toggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 
+
 // ================= SCROLL SUAVE =================
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
+
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -16,5 +18,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
 
         navMenu.classList.remove('active');
+
     });
+});
+
+
+// ================= ANIMAÇÃO AO ROLAR =================
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+
+document.querySelectorAll('.section').forEach(section => {
+
+    section.classList.add('hidden');
+    observer.observe(section);
+
 });
